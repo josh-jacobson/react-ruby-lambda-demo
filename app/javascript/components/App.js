5eed45853ca61d55ece56aab69a3c48a6a2928f1
Josh Jacobson <joshjacobson14@gmail.com>
2020-04-10 11:39:45 -0700

    basic keyboard demo, first commit

---
+++ app/javascript/components/App.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Piano, KeyboardShortcuts, MidiNumbers } from 'react-piano';
+
+import DimensionsProvider from './DimensionsProvider';
+import SoundfontProvider from './SoundfontProvider';
+
+// webkitAudioContext fallback needed to support Safari
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const soundfontHostname = 'https://d1pzp51pvbm36p.cloudfront.net';
+
+const noteRange = {
+  first: MidiNumbers.fromNote('c3'),
+  last: MidiNumbers.fromNote('f4'),
+};
+const keyboardShortcuts = KeyboardShortcuts.create({
+  firstNote: noteRange.first,
+  lastNote: noteRange.last,
+  keyboardConfig: KeyboardShortcuts.HOME_ROW,
+});
+
+function App() {
+  return (
+    <div>
+      <h1>Lambda school Demo with react-piano</h1>
+      <div className="mt-5">
+        <p>Basic piano with hardcoded width</p>
+        <BasicPiano />
+      </div>
+
+      <div className="mt-5">
+        <p>
+          Responsive piano which resizes to container's width. Try resizing the
+          window!
+        </p>
+        <ResponsivePiano />
+      </div>
+    </div>
+  );
+}
+
+function BasicPiano() {
+  return (
+    <SoundfontProvider
+      instrumentName="acoustic_grand_piano"
+      audioContext={audioContext}
+      hostname={soundfontHostname}
+      render={({ isLoading, playNote, stopNote }) => (
+        <Piano
+          noteRange={noteRange}
+          width={500}
+          playNote={playNote}
+          stopNote={stopNote}
+          disabled={isLoading}
+          keyboardShortcuts={keyboardShortcuts}
+        />
+      )}
+    />
+  );
+}
+
+function ResponsivePiano(props) {
+  return (
+    <DimensionsProvider>
+      {({ containerWidth, containerHeight }) => (
+        <SoundfontProvider
+          instrumentName="acoustic_grand_piano"
+          audioContext={audioContext}
+          hostname={soundfontHostname}
+          render={({ isLoading, playNote, stopNote }) => (
+            <Piano
+              noteRange={noteRange}
+              width={containerWidth}
+              playNote={playNote}
+              stopNote={stopNote}
+              disabled={isLoading}
+              {...props}
+            />
+          )}
+        />
+      )}
+    </DimensionsProvider>
+  );
+}
+
+export default App
